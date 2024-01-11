8e3792b8a46c6cfb9451eee9f9b02bbdf82ffd38
Joshua Peek <josh@users.noreply.github.com>
2024-01-11 20:10:28 +0000

    Prompt for tmdb api key

--- custom_components/trakt/config_flow.py
+++ custom_components/trakt/config_flow.py
@@ -3,12 +3,19 @@
 import logging
 from typing import Any
 
+import voluptuous as vol
 from aiohttp import ClientSession
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers import config_entry_oauth2_flow
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN
+
+STEP_TMDB_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Optional("tmdb_api_key"): vol.All(str, vol.Length(min=32, max=32)),
+    }
+)
 
 
 class OAuth2FlowHandler(
@@ -17,6 +24,7 @@ class OAuth2FlowHandler(
     """Config flow to handle Trakt OAuth2 authentication."""
 
     DOMAIN = DOMAIN
+    data: dict[str, Any]
 
     @property
     def logger(self) -> logging.Logger:
@@ -24,16 +32,36 @@ class OAuth2FlowHandler(
         return logging.getLogger(__name__)
 
     async def async_oauth_create_entry(self, data: dict[str, Any]) -> FlowResult:
+        self.data = data
+
         profile = await trakt_user_profile(
             async_get_clientsession(self.hass),
             self.flow_impl.client_id,
             data["token"]["access_token"],
         )
         data["username"] = profile["username"]
+        logging.info("Trakt username: %s", data["username"])
 
-        await self.async_set_unique_id(unique_id=profile["username"])
+        return await self.async_step_tmdb()
 
-        return await super().async_oauth_create_entry(data)
+    async def async_step_tmdb(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        if user_input is None:
+            return self.async_show_form(
+                step_id="tmdb",
+                data_schema=STEP_TMDB_DATA_SCHEMA,
+            )
+
+        self.data["tmdb_api_key"] = user_input.get("tmdb_api_key", None)
+        if not self.data["tmdb_api_key"]:
+            self.logger.warning("No TMDB API key provided")
+
+        return await self._create_entry()
+
+    async def _create_entry(self) -> FlowResult:
+        await self.async_set_unique_id(unique_id=self.data["username"])
+        return self.async_create_entry(title=self.flow_impl.name, data=self.data)
 
 
 async def trakt_user_profile(
